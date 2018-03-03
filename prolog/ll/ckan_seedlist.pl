19f088cf5111edac58dabd8b5531744b11bde89f
Wouter Beek <wouter@triply.cc>
2018-03-04 00:53:17 +0100

    ENHANCED: Registered more CKAN formats and Media Types.

--- prolog/ll/ckan_seedlist.pl
+++ prolog/ll/ckan_seedlist.pl
@@ -150,14 +150,19 @@ clean_media_type(Format1, MediaType) :-
           print_message(warning, unknown_media_type(MediaType0)),
           fail
       )
-  ;   (   % knwon known format
-          format_media_type_(Format3, MediaType)
+  ;   % Remove the leading dot, if any.
+      (   sub_atom(Format3, 0, 1, _, .)
+      ->  sub_atom(Format3, 1, _, 0, Format4)
+      ;   Format4 = Format3
+      ),
+      (   % knwon known format
+          format_media_type_(Format4, MediaType)
       ->  true
       ;   % known unknown format
-          format_(Format3)
+          format_(Format4)
       ->  fail
       ;   % unknown unknown format
-          print_message(warning, unknown_format(Format3)),
+          print_message(warning, unknown_format(Format4)),
           fail
       )
   ).
@@ -176,7 +181,19 @@ format_(Format) :-
   sub_atom(Format, _, _, 0, '.topojson').
 format_(Format) :-
   sub_atom(Format, _, _, 0, '.txt').
+format_(api).
 format_('apping.owl').
+format_(arcgis).
+format_(ascii).
+format_(asp).
+format_(aspx).
+format_(bin).
+format_(biopax).
+format_(classifide).
+format_(creole).
+format_('conll-u').
+format_(cvs).
+format_(data).
 format_('data file in asc').
 format_('data file in csv').
 format_('data file in csv, excel').
@@ -189,61 +206,61 @@ format_('data file in tifgis').
 format_('doc:04').
 format_('esri arc export').
 format_('esri grid').
+format_('esri rest').
 format_('esri shape file').
 format_('esri shape files').
 format_('esri shape').
-format_('google doc').
-format_('google spreadsheet').
-format_('gtfs-realtime').
-format_('gzip:text/sql').
-format_('linked data').
-format_('marc21').
-format_('ms acess mdb').
-format_('ms excel csv').
-format_('nc:55').
-format_('owl, ontology, meta/owl').
-format_('plain text').
-format_('png jpg').
-format_('rdf endpoint').
-format_('sisis export format').
-format_('sparql-query').
-format_(api).
-format_(arcgis).
-format_(ascii).
-format_(asp).
-format_(aspx).
-format_(bin).
-format_(biopax).
-format_(creole).
-format_('conll-u').
 format_(gdocs).
+format_(geopackage).
 format_(geopdf).
+format_(getdata).
 format_(gis).
 format_(git).
+format_('google doc').
+format_('google spreadsheet').
 format_(googlespreadsheet).
 format_(gtfs).
+format_('gtfs-realtime').
+format_('gzip:text/sql').
 format_(hydra).
+format_(hyperlink).
 format_(img).
 format_(index).
 format_(iso).
 format_(labpal).
 format_(link).
+format_('linked data').
 format_(mabxml).
 format_(map).
+format_('marc21').
 format_(mol).
+format_('ms acess mdb').
+format_('ms excel csv').
+format_('nc:55').
 format_(netcdf).
+format_(none).
 format_(osm).
+format_(other).
 format_(owl).
+format_('owl, ontology, meta/owl').
 format_(pbf).
 format_(php).
+format_('plain text').
+format_('png jpg').
+format_(qgis).
 format_(rdf).
+format_('rdf endpoint').
 format_(scraper).
 format_(sdf).
 format_(search).
 format_(shape).
+format_(shapefiles).
 format_(shp).
+format_(shx).
+format_('sisis export format').
 format_(solr).
 format_(sparql).
+format_('sparql-query').
 format_(spreadsheet).
 format_(sql).
 format_(sqlite).
@@ -256,41 +273,35 @@ format_(tmx).
 format_(tomtom).
 format_(topojson).
 format_(txt).
+format_(url).
 format_(void).
+format_(wcs).
+format_(wfs).
 format_(wms).
 format_(wsdl).
+format_(xlb).
 format_('xml, json, rdf').
 format_(xsd).
 format_(yml).
 
 format_media_type_(Format, MediaType) :-
   media_type_extension(MediaType, Format).
-format_media_type_(Format, media(application/msword,[])) :-
-  sub_atom(Format, _, _, 0, '.doc').
-format_media_type_(Format, media(application/'vnd.geo+json',[])) :-
-  sub_atom(Format, _, _, 0, '.geojson').
-format_media_type_(Format, media(application/'gml+xml',[])) :-
-  sub_atom(Format, _, _, 0, '.gml').
-format_media_type_(Format, text/html) :-
-  sub_atom(Format, _, _, 0, '.html').
-format_media_type_(Format, media(application/json,[])) :-
-  sub_atom(Format, _, _, 0, '.json').
-format_media_type_(Format, media(image/png,[])) :-
-  sub_atom(Format, _, _, 0, '.png').
-format_media_type_(Format, media(text/'tab-separated-values',[])) :-
-  sub_atom(Format, _, _, 0, '.tab').
-format_media_type_(Format, media(application/'x-tar',[])) :-
-  sub_atom(Format, _, _, 0, '.tgz').
-format_media_type_(Format, media(application/zip,[])) :-
-  sub_atom(Format, _, _, 0, '.zip').
 format_media_type_('atom feed',                 media(application/'atom+x',[])).
+format_media_type_(biopax,                      media(application/'vnd.biopax.rdf+xml',[])).
 format_media_type_('bz2:nt',                    media(application/'n-triples',[])).
 format_media_type_('bz2:xml',                   media(application/xml,[])).
 format_media_type_('data file in excel',        media(application/'vnd.ms-excel',[])).
+format_media_type_(geojsom,                     media(application/'vnd.geo+json',[])).
+format_media_type_(georss,                      media(application/'rss+xml',[])).
+format_media_type_(geotiff,                     media(image/tiff,[])).
+format_media_type_(gzip,                        media(application/gzip,[])).
 format_media_type_('gzip::nquads',              media(application/'n-quads',[])).
 format_media_type_('gzip:ntriples',             media(application/'n-triples',[])).
 format_media_type_('html+rdfa',                 media(application/'xhtml+xml',[])).
+format_media_type_(html5,                       media(text/html,[])).
+format_media_type_(jpg,                         media(image/jpeg,[])).
 format_media_type_('json-ld',                   media(application/'ld+json',[])).
+format_media_type_('microsoft access',          media(application/'vnd.ms-access',[])).
 format_media_type_('microsoft access database', media(application/'vnd.ms-access',[])).
 format_media_type_('microsoft excel',           media(application/'vnd.ms-excel',[])).
 format_media_type_('ms access',                 media(application/'vnd.ms-access',[])).
@@ -303,8 +314,11 @@ format_media_type_('rdf+xml',                   media(application/'rdf+xml',[]))
 format_media_type_('rdf-n3',                    media(text/n3,[])).
 format_media_type_('rdf-turtle',                media(text/turtle,[])).
 format_media_type_('rdf-xml',                   media(application/'rdf+xml',[])).
+format_media_type_(rdfa,                        media(application/'xhtml+xml',[])).
+format_media_type_(rdfxml,                      media(application/'rdf+xml',[])).
 format_media_type_('sparql-json',               media(application/'sparql-results+json',[])).
 format_media_type_('tar.gz',                    media(application/'x-tar',[])).
+format_media_type_(tif,                         media(image/tiff,[])).
 format_media_type_('trig gzip',                 media(application/trig,[])).
 format_media_type_('ttl.bz2',                   media(text/turtle,[])).
 format_media_type_('ttl.bzip2',                 media(text/turtle,[])).
@@ -312,13 +326,6 @@ format_media_type_('xls (zip)',                 media(application/'vnd.ms-excel'
 format_media_type_('xml (zipped)',              media(text/xml,[])).
 format_media_type_('zip archive',               media(application/zip,[])).
 format_media_type_('zip:csv',                   media(text/csv,[])).
-format_media_type_(biopax,                      media(application/'vnd.biopax.rdf+xml',[])).
-format_media_type_(georss,                      media(application/'rss+xml',[])).
-format_media_type_(geotiff,                     media(image/tiff,[])).
-format_media_type_(gzip,                        media(application/gzip,[])).
-format_media_type_(html5,                       media(text/html,[])).
-format_media_type_(rdfa,                        media(application/'xhtml+xml',[])).
-format_media_type_(rdfxml,                      media(application/'rdf+xml',[])).
 format_media_type_('sparql-xml',                media(application/'sparql-results+xml',[])).
 format_media_type_(tab,                         media(text/'tab-separated-values',[])).
 format_media_type_(tar,                         media(application/'x-tar',[])).
@@ -355,54 +362,55 @@ media_type_(media(rdf/'xml, html, json',_)).
 media_type_(media(rdf/void,_)).
 media_type_(media(text/plain,_)).
 
-media_type_media_type_(MediaType, MediaType) :-
-  media_type(MediaType).
+media_type_media_type_(media(Super/Sub,_), media(Super/Sub,Params)) :-
+  media_type(media(Super/Sub,Params)).
 media_type_media_type_(media(application/csv,L),               media(text/csv,L)).
-media_type_media_type_(media(application/xml,L),               media(text/xml,L)).
+media_type_media_type_(media(application/msexcel,L),           media(application/'vnd.ms-excel',L)).
+media_type_media_type_(media(application/rar,L),               media(application/'vnd.rar',L)).
+media_type_media_type_(media(application/'rdf xml',L),         media(application/'rdf+xml',L)).
 media_type_media_type_(media(application/'x-bzip',L),          media(application/'x-bzip2',L)).
 media_type_media_type_(media(application/'x-bzip2',L),         media(application/'x-bzip2',L)).
-media_type_media_type_(media(application/'x-pdf',L),           media(application/pdf,L)).
-media_type_media_type_(media(application/'x-tgz',L),           media(application/'x-tar',L)).
-media_type_media_type_(media(application/'xml+atom',L),        media(application/'atom+x',L)).
-media_type_media_type_(media(html/doc,L),                      media(text/html,L)).
-media_type_media_type_(media(kml/kmz,L),                       media(application/'vnd.google-earth.kml+xml',L)).
-media_type_media_type_(media(marc/xml,L),                      media(text/xml,L)).
-media_type_media_type_(media(text/sql,L),                      media(application/sql,L)).
-media_type_media_type_(media(text/'x-csv',L),                  media(text/csv,L)).
-media_type_media_type_(media(wfs/gml,L),                       media(application/'gml+xml',L)).
-media_type_media_type_(media(zip/json,L),                      media(application/json,L)).
-media_type_media_type_(media(zip/tsv,L),                       media(text/'tab-separated-values',L)).
-media_type_media_type_(media(application/'rdf xml',L),         media(application/'rdf+xml',L)).
 media_type_media_type_(media(application/'x-gzip',L),          media(application/gzip,L)).
 media_type_media_type_(media(application/'x-nquads',L),        media(application/'n-quads',L)).
 media_type_media_type_(media(application/'x-ntriples',L),      media(application/'n-triples',L)).
+media_type_media_type_(media(application/'x-pdf',L),           media(application/pdf,L)).
+media_type_media_type_(media(application/'x-tgz',L),           media(application/'x-tar',L)).
 media_type_media_type_(media(application/'x-trig',L),          media(application/trig,L)).
 media_type_media_type_(media(application/'x-turtle',L),        media(text/turtle,L)).
 media_type_media_type_(media(application/'x-vnd.oasis.opendocument.spreadsheet',[]), media(application/'vnd.oasis.opendocument.spreadsheet',[])).
+media_type_media_type_(media(application/xml,L),               media(text/xml,L)).
+media_type_media_type_(media(application/'xml+atom',L),        media(application/'atom+x',L)).
 media_type_media_type_(media(application/'xml+rdf',L),         media(application/'rdf+xml',L)).
 media_type_media_type_(media(example/'application/rdf+xml',L), media(application/'rdf+xml',L)).
-media_type_media_type_(media(example/n3,L),                    media(text/n3,L)).
-media_type_media_type_(media(example/ntriples,L),              media(application/'n-triples',L)).
 media_type_media_type_(media(example/'html+rdfa',L),           media(application/'xhtml+xml',L)).
 media_type_media_type_(media(example/'rdf xml',L),             media(application/'rdf+xml',L)).
 media_type_media_type_(media(example/'rdf+json',L),            media(application/'ld+json',L)).
 media_type_media_type_(media(example/'rdf+ttl',L),             media(text/turtle,L)).
 media_type_media_type_(media(example/'rdf+xml',L),             media(application/'rdf+xml',L)).
+media_type_media_type_(media(example/'x-turtle',L),            media(text/turtle,L)).
+media_type_media_type_(media(example/n3,L),                    media(text/n3,L)).
+media_type_media_type_(media(example/ntriples,L),              media(application/'n-triples',L)).
 media_type_media_type_(media(example/rdfa,L),                  media(application/'xhtml+xml',L)).
 media_type_media_type_(media(example/turtle,L),                media(text/turtle,L)).
-media_type_media_type_(media(example/'x-turtle',L),            media(text/turtle,L)).
+media_type_media_type_(media(html/doc,L),                      media(text/html,L)).
 media_type_media_type_(media(html/rdf,L),                      media(application/'xhtml_xml',L)).
-media_type_media_type_(media(rdf/n3,L),                        media(text/n3,L)).
+media_type_media_type_(media(kml/kmz,L),                       media(application/'vnd.google-earth.kml+xml',L)).
+media_type_media_type_(media(marc/xml,L),                      media(text/xml,L)).
 media_type_media_type_(media(rdf/'n-triples',L),               media(application/'n-triples',L)).
+media_type_media_type_(media(rdf/'xml example',L),             media(application/'rdf+xml',L)).
+media_type_media_type_(media(rdf/n3,L),                        media(text/n3,L)).
 media_type_media_type_(media(rdf/nt,L),                        media(application/'n-triples',L)).
 media_type_media_type_(media(rdf/turtle,L),                    media(text/turtle,L)).
-media_type_media_type_(media(rdf/'xml example',L),             media(application/'rdf+xml',L)).
+media_type_media_type_(media(sparql/json,L),                   media(application/'sparql-results+json',L)).
+media_type_media_type_(media(sparql/xml,L),                    media(application/'sparql-results+xml',L)).
 media_type_media_type_(media(text/'comma-separated-values',L), media(text/csv,L)).
-media_type_media_type_(media(text/javascript,L),               media(application/javascript,L)).
-media_type_media_type_(media(text/n3,L),                       media(text/n3,L)).
 media_type_media_type_(media(text/'rdf+n3',L),                 media(text/n3,L)).
 media_type_media_type_(media(text/'rdf+ttl',L),                media(text/turtle,L)).
-media_type_media_type_(media(sparql/json,L),                   media(application/'sparql-results+json',L)).
-media_type_media_type_(media('sparql-xml',L),                  media(application/'sparql-results+xml',L)).
-media_type_media_type_(media(sparql/xml,L),                    media(application/'sparql-results+xml',L)).
+media_type_media_type_(media(text/'x-csv',L),                  media(text/csv,L)).
+media_type_media_type_(media(text/javascript,L),               media(application/javascript,L)).
+media_type_media_type_(media(text/n3,L),                       media(text/n3,L)).
+media_type_media_type_(media(text/sql,L),                      media(application/sql,L)).
+media_type_media_type_(media(wfs/gml,L),                       media(application/'gml+xml',L)).
 media_type_media_type_(media(xml/rdf,L),                       media(application/'rdf+xml',L)).
+media_type_media_type_(media(zip/json,L),                      media(application/json,L)).
+media_type_media_type_(media(zip/tsv,L),                       media(text/'tab-separated-values',L)).
--- run.pl
+++ run.pl
@@ -1,4 +1,16 @@
+:- use_module(library(aggregate)).
+:- use_module(library(thread)).
+
+:- use_module(library(http/ckan_api)).
 :- use_module(library(http/http_unix_daemon)).
 :- use_module(library(http/thread_httpd)).
 
+:- use_module(library(ll/ckan_seedlist)).
 :- use_module(library(ll/ll_seedlist_server)).
+
+populate_seedlist :-
+  aggregate_all(set(Uri), ckan_site_uri(Uri), Uris),
+  %concurrent_maplist(ckan_scrape_init, Uris),
+  member(Uri, Uris),
+  writeln(Uri),
+  ckan_scrape_init(Uri).

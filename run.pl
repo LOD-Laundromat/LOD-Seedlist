9af84f2ca3d622541e10ea459681adae5cc0bf7a
Wouter Beek <wouter@triply.cc>
2018-03-18 20:53:26 +0100

    ENHANCED: Use thread support from PLC.

--- run.pl
+++ run.pl
@@ -1,12 +1,13 @@
-:- use_module(library(http/ckan_api)).
+:- use_module(library(aggregate)).
 :- use_module(library(http/http_unix_daemon)).
 :- use_module(library(http/thread_httpd)).
 
+:- use_module(library(http/ckan_api)).
 :- use_module(library(ll/ckan_seedlist)).
 :- use_module(library(ll/ll_seedlist_server)).
+:- use_module(library(thread_ext)).
 
 populate_seedlist :-
-  forall(
-    ckan_site_uri(Uri),
-    thread_create(ckan_scrape_init(Uri), _, [alias(Uri)])
-  ).
+  thread_monitor,
+  aggregate_all(set(Uri), ckan_site_uri(Uri), Uris),
+  threaded_maplist(4, ckan_scrape_init, Uris).

6b94faaf994af1fe98336cb5643bedce1499851f
Wouter Beek <wouter@triply.cc>
2018-04-19 21:12:16 +0200

    ENHANCED: Scripts in `/script' and configuration in `/conf'.

---
+++ script/test-server.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+swipl -q ../run-server.pl --port=4004 --interactive --conf=../conf/conf.json --workers=8
--- test-server.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-swipl -q run-server.pl --port=4004 --interactive --conf=conf.json --workers=8

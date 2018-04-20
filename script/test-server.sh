894453430aa91d2b59399089cafe914203b7eddf
Wouter Beek <wouter@triply.cc>
2018-04-20 11:53:47 +0200

    FIXED: Typo in test script.
    ENHANCED: Updated example service file.

--- script/test-server.sh
+++ script/test-server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-swipl -q run-server.pl --port=4004 --interactive --conf=../conf/conf.json --workers=8
+swipl -q run_server.pl --port=4004 --interactive --conf=../conf/conf.json --workers=8
